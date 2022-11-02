223626765fc65f20d48e16b36c02a0f000fe4038
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2022-11-02 21:16:58 +0000

    style

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="container">
       <div className="grid">
+        <div className="display"></div>
         <div className="padButton AC">AC</div>
         <div className="padButton C">C</div>
         <div className="padButton div">/</div>
